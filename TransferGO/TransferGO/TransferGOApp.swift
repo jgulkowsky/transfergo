e4ec71ef477bc9aad30ae251f7bb483913df6f5c
jgulkowsky <jgulkowsky@gmail.com>
2023-08-14 11:22:03 +0200

    project init / git init / add gitignore

---
+++ TransferGO/TransferGO/TransferGOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransferGOApp.swift
+//  TransferGO
+//
+//  Created by Jan Gulkowski on 14/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TransferGOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
