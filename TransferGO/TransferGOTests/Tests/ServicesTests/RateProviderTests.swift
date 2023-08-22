efad0a79d617cfcca144c9dc7f35fcbf8454f056
jgulkowsky <jgulkowsky@gmail.com>
2023-08-23 01:05:59 +0200

    fix RateProviderTests

--- TransferGO/TransferGOTests/Tests/ServicesTests/RateProviderTests.swift
+++ TransferGO/TransferGOTests/Tests/ServicesTests/RateProviderTests.swift
@@ -7,8 +7,6 @@
 
 import XCTest
 
-// todo: this tests are wrong - we don't wait for Tasks to finish - checkout SelectCountryViewModelTests for proper approach
-
 final class RateProviderTests: XCTestCase {
     private var rateProvider: RateProvider!
     private var mockURLProvider: MockURLProvider!
@@ -37,75 +35,67 @@ final class RateProviderTests: XCTestCase {
         )
     }
     
-    func test_whenGetRateIsCalled_andURLProviderThrows_thenRateProviderThrowsToo_andOnlyURLProviderWasCalled() {
-        Task {
-            do {
-                mockURLProvider.shouldThrow = true
-                _ = try await rateProvider.getRate(
-                    from: PredefinedCountry.poland,
-                    to: PredefinedCountry.ukraine,
-                    amount: 1000.0
-                )
-                XCTFail("it should throw but it returned")
-            } catch {
-                XCTAssert(true)
-                XCTAssertTrue(mockURLProvider.wasCalled)
-                XCTAssertFalse(mockRequestHandler.wasCalled)
-                XCTAssertFalse(mockResponseHandler.wasCalled)
-            }
+    func test_whenGetRateIsCalled_andURLProviderThrows_thenRateProviderThrowsToo_andOnlyURLProviderWasCalled() async {
+        do {
+            mockURLProvider.shouldThrow = true
+            _ = try await rateProvider.getRate(
+                from: PredefinedCountry.poland,
+                to: PredefinedCountry.ukraine,
+                amount: 1000.0
+            )
+            XCTFail("it should throw but it returned")
+        } catch {
+            XCTAssert(true)
+            XCTAssertTrue(mockURLProvider.wasCalled)
+            XCTAssertFalse(mockRequestHandler.wasCalled)
+            XCTAssertFalse(mockResponseHandler.wasCalled)
         }
     }
     
-    func test_whenGetRateIsCalled_andRequestHandlerThrows_thenRateProviderThrowsToo_andBoth_URLProvider_andRequestHandlerWereCalled() {
-        Task {
-            do {
-                mockRequestHandler.shouldThrow = true
-                _ = try await rateProvider.getRate(
-                    from: PredefinedCountry.poland,
-                    to: PredefinedCountry.ukraine,
-                    amount: 1000.0
-                )
-                XCTFail("it should throw but it returned")
-            } catch {
-                XCTAssert(true)
-                XCTAssertTrue(mockURLProvider.wasCalled)
-                XCTAssertTrue(mockRequestHandler.wasCalled)
-                XCTAssertFalse(mockResponseHandler.wasCalled)
-            }
+    func test_whenGetRateIsCalled_andRequestHandlerThrows_thenRateProviderThrowsToo_andBoth_URLProvider_andRequestHandlerWereCalled() async {
+        do {
+            mockRequestHandler.shouldThrow = true
+            _ = try await rateProvider.getRate(
+                from: PredefinedCountry.poland,
+                to: PredefinedCountry.ukraine,
+                amount: 1000.0
+            )
+            XCTFail("it should throw but it returned")
+        } catch {
+            XCTAssert(true)
+            XCTAssertTrue(mockURLProvider.wasCalled)
+            XCTAssertTrue(mockRequestHandler.wasCalled)
+            XCTAssertFalse(mockResponseHandler.wasCalled)
         }
     }
     
-    func test_whenGetRateIsCalled_andRequestHandlerThrows_thenRateProviderThrowsToo_andAll_URLProvider_requestHandler_andResponseHandlerWereCalled() {
-        Task {
-            do {
-                mockResponseHandler.shouldThrow = true
-                _ = try await rateProvider.getRate(
-                    from: PredefinedCountry.poland,
-                    to: PredefinedCountry.ukraine,
-                    amount: 1000.0
-                )
-                XCTFail("it should throw but it returned")
-            } catch {
-                XCTAssert(true)
-                XCTAssertTrue(mockURLProvider.wasCalled)
-                XCTAssertTrue(mockRequestHandler.wasCalled)
-                XCTAssertTrue(mockResponseHandler.wasCalled)
-            }
+    func test_whenGetRateIsCalled_andRequestHandlerThrows_thenRateProviderThrowsToo_andAll_URLProvider_requestHandler_andResponseHandlerWereCalled() async {
+        do {
+            mockResponseHandler.shouldThrow = true
+            _ = try await rateProvider.getRate(
+                from: PredefinedCountry.poland,
+                to: PredefinedCountry.ukraine,
+                amount: 1000.0
+            )
+            XCTFail("it should throw but it returned")
+        } catch {
+            XCTAssert(true)
+            XCTAssertTrue(mockURLProvider.wasCalled)
+            XCTAssertTrue(mockRequestHandler.wasCalled)
+            XCTAssertTrue(mockResponseHandler.wasCalled)
         }
     }
     
-    func test_whenGetRateIsCalled_andNooneThrows_thenRateProviderReturnsRateFromResponseHandler() {
-        Task {
-            do {
-                let rate = try await rateProvider.getRate(
-                    from: PredefinedCountry.poland,
-                    to: PredefinedCountry.ukraine,
-                    amount: 1000.0
-                )
-                XCTAssertEqual(rate, TestHelper.getRate())
-            } catch {
-                XCTFail("it should not throw")
-            }
+    func test_whenGetRateIsCalled_andNooneThrows_thenRateProviderReturnsRateFromResponseHandler() async {
+        do {
+            let rate = try await rateProvider.getRate(
+                from: PredefinedCountry.poland,
+                to: PredefinedCountry.ukraine,
+                amount: 1000.0
+            )
+            XCTAssertEqual(rate, TestHelper.getRate())
+        } catch {
+            XCTFail("it should not throw")
         }
     }
 }
