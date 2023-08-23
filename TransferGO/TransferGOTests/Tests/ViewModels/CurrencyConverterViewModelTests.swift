7e342c0b72c0647265f0793cd82d4b0f209abee8
jgulkowsky <jgulkowsky@gmail.com>
2023-08-23 02:14:26 +0200

    add todos for tomorrow

--- TransferGO/TransferGOTests/Tests/ViewModels/CurrencyConverterViewModelTests.swift
+++ TransferGO/TransferGOTests/Tests/ViewModels/CurrencyConverterViewModelTests.swift
@@ -35,8 +35,6 @@ final class CurrencyConverterViewModelTests: XCTestCase {
         )
     }
     
-    // todo: test_givenThatInfoContainsPolandUkraineAnd300
-    
     func test_givenThatInfoThatIsPassedOnInitContainsFromPolandToUkraineWithAmount300_whenViewModelIsInitialized_then_fromCountryIsPoland_toCountryIsUkraine_fromAmountIs300With2decimalPlaces_currentRateIsNil_andGetCurrentRateErrorIsNil() {
         // then
         XCTAssertEqual(viewModel.fromCountry, PredefinedCountry.poland)
@@ -45,4 +43,44 @@ final class CurrencyConverterViewModelTests: XCTestCase {
         XCTAssertNil(viewModel.currentRate)
         XCTAssertNil(viewModel.getCurrentRateError)
     }
+    
+    // todo: test about tryToUpdateCurrentRate on init
+    //  todo: also when requirements are not satisfied
+    //  todo: also when requirements are satisfied
+    //  todo: also when requirements are satisfied but error is thrown (URLError.cancelled, CancellationError, other)
+    //  todo: also when requirements are satisfied and there's no error but we get the rate
+    //  todo: also when you tryToUpdateCurrentRate multiple times
+    
+    // todo: test about onSceneActive
+    // todo: test about onSceneInactive
+    // todo: test about onSceneInBackground
+    
+    // todo: test about sendFromTapped
+    // todo: test about sendToTapped
+    // todo: test about switchTapped
+    // todo: test about backgroundTapped
+    
+    // todo: test about setting up fromCountry can change value in toCountry
+    // todo: test about setting up toCountry can change value in fromCountry
+    
+    // todo: test about setting up fromCountry checksLimits
+    // todo: test about setting up fromAmount checksLimits
+    
+    // todo: test about setting up fromCountry tries to updateCurrentRate
+    // todo: test about setting up toCountry tries to updateCurrentRate
+    // todo: test about setting up fromAmount tries to updateCurrentRate
+    
+    // todo: test about setting up toAmount (maybe covered in above tests...)
+    // todo: test about setting up fromAmountFocused (maybe covered in above tests...)
+    // todo: test about setting up currentRate (maybe covered in above tests...)
+    
+    // todo: test about connectionError (maybe covered in above tests...)
+    // todo: test about limitExceededError (maybe covered in above tests...)
+    // todo: test about getCurrentRateError (maybe covered in above tests...)
+    
+    // todo: test about shouldEnableFields (maybe covered in above tests...)
+    // todo: test about limitExceeded (maybe covered in above tests...)
+    
+    // todo: test about regularCurrentRateUpdates
+    // todo: test about gettingNetworkStatus
 }
